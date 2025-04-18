2614589ac2743a7560205427ec55c7ec340d3e7b
merge-simpson <letsdev@kakao.com>
2025-04-19 02:35:30 +0900

    feat(demo): scan configuration properties

--- src/main/java/nettee/demo/DemoJwtAuthApplication.java
+++ src/main/java/nettee/demo/DemoJwtAuthApplication.java
@@ -2,8 +2,10 @@ package nettee.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "nettee")
+@ConfigurationPropertiesScan(basePackages = "nettee")
 public class DemoJwtAuthApplication {
 
     public static void main(String[] args) {
