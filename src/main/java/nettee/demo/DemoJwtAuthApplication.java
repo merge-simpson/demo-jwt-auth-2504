3f8e6c2eebff68e37b57f429c221515709b44d94
merge-simpson <letsdev@kakao.com>
2025-04-19 02:27:43 +0900

    initial commit

---
+++ src/main/java/nettee/demo/DemoJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package nettee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtAuthApplication.class, args);
+    }
+
+}
